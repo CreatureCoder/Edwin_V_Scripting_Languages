31e881e46549ea59f3801f6e7a0e7c75ec418ed5
CreatureCoder <edvahl01@wsc.edu>
2023-01-24 17:55:31 -0600

    Put a start to the Salary Calculator with a start and a test with the input

---
+++ Module_2_SalaryCalaculator/Module2_SalaryCalculator.py
@@ -0,0 +1,10 @@
+Max_Length = 40
+Column_Length = 30
+
+print("=" * Max_Length)
+print(" The Salary Calculator Program")
+print("=" * Max_Length)
+
+AnyVariables = float (input(f"{' Salary per hour':.<{Column_Length}}"))
+
+print(AnyVariables)
