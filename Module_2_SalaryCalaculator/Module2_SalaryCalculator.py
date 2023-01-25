b157db67ba2b5d8dbf28bedb31a58023b418cba8
CreatureCoder <edvahl01@wsc.edu>
2023-01-24 18:15:48 -0600

    got the expected inputs to work except for the ":" to be added.

--- Module_2_SalaryCalaculator/Module2_SalaryCalculator.py
+++ Module_2_SalaryCalaculator/Module2_SalaryCalculator.py
@@ -1,10 +1,21 @@
+
 Max_Length = 40
 Column_Length = 30
 
+
 print("=" * Max_Length)
 print(" The Salary Calculator Program")
-print("=" * Max_Length)
+print("=" * Max_Length, end="\n\n")
 
-AnyVariables = float (input(f"{' Salary per hour':.<{Column_Length}}"))
 
-print(AnyVariables)
+Salary_Hour = float (input(f"{'  Salary per hour':.<{Column_Length}}",))
+Hours_Week = float (input(f"{'  Hours per week':.<{Column_Length}}",))
+Holidays = float (input(f"{'  Holidays per year':.<{Column_Length}}",))
+Vacations = float (input(f"{'  Vacation days per year':.<{Column_Length}}",))
+print("\n", "=" * Max_Length)
+
+
+print(Salary_Hour)
+print(Hours_Week)
+print(Holidays)
+print(Vacations)
