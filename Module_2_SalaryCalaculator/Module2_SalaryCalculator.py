b8833b1b769d128aeff5a56306f6d0d03e3b96d2
CreatureCoder <edvahl01@wsc.edu>
2023-01-25 17:47:19 -0600

    Need to eat

--- Module_2_SalaryCalaculator/Module2_SalaryCalculator.py
+++ Module_2_SalaryCalaculator/Module2_SalaryCalculator.py
@@ -5,24 +5,18 @@ Column_Length = 30
 
 print("=" * Max_Length)
 print(" The Salary Calculator Program")
-print("=" * Max_Length, end="\n\n")
+print("=" * Max_Length)
 
 
-Salary_Hour = float (input(f"{'  Salary per hour':.<{Column_Length}}",))
-Hours_Week = float (input(f"{'  Hours per week':.<{Column_Length}}",))
-Days_Week = float (input(f"{'  Days per week':.<{Column_Length}}",))
-Holidays = float (input(f"{'  Holidays per year':.<{Column_Length}}",))
-Vacations = float (input(f"{'  Vacation days per year':.<{Column_Length}}",))
-print("\n", "=" * Max_Length)
+Salary_Hour = float (input(f"{'  Salary per hour':.<{Column_Length}}: ",))
+Hours_Week = float (input(f"{'  Hours per week':.<{Column_Length}}: ",))
+Days_Week = float (input(f"{'  Days per week':.<{Column_Length}}: ",))
+Holidays = float (input(f"{'  Holidays per year':.<{Column_Length}}: ",))
+Vacations = float (input(f"{'  Vacation days per year':.<{Column_Length}}: ",))
+print("=" * Max_Length)
 
+Unadjusted_Salary = (Salary_Hour * Hours_Week * 52)
+Adjusted_Salary = (Salary_Hour * 8 * (Days_Week * 52 - Holidays - Vacations))
 
-Unadjusted_Wage = Salary_Hour * Hours_Week * (365 - Days_Week * 52 + 1)
-adjusted_Wage = Salary_Hour * Hours_Week * ((365 - Days_Week * 52 + 1) - Holidays - Vacations)
-
-print = (Unadjusted_Wage)
-print = (adjusted_Wage)
-
-13.3
-40
-5
-
+print(f"{' Your Unadjusted Salary':.<{Column_Length}}: ${Unadjusted_Salary:6,.2f}")
+print(f"{' Your Adjusted Salary':.<{Column_Length}}: ${Adjusted_Salary:6,.2f}")
