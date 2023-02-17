6a55bb0dc646fb44e8153a9a4dbae91ca60d16d4
CreatureCoder <edvahl01@wsc.edu>
2023-02-17 14:34:19 -0600

    I have finished the delete_student and finished most if not finished update_student, so you can enter blank to keep the last or first name.  I still need to do the comments on all of them thought.

--- Module_4_Student_Maintenance/student_maintenance.py
+++ Module_4_Student_Maintenance/student_maintenance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Students adds the modifier to the main menuu
+Students adds the modifier to the main menu
 """
 
 import data_validation as dv
@@ -42,19 +42,68 @@ def update_student(students):
     student = students[student_id]
     first_name, last_name = student.values()
 
-    first_update_name = dv.get_string("please enter the student's update first name")
-    last_update_name = dv.get_string("Please enter the student's update last name")
+    print(f'The student chosen to be updated is {first_name} {last_name}\n')
 
-    yes_or_no = dv.get_yes_no(f'Please confirm updating {first_name} {last_name} to {first_update_name} '
-                              f'{last_update_name}')
-    if yes_or_no == False:
+    first_update_name = input("Please enter the student's update first name or press enter to keep "
+                              + first_name + ": ")
+    last_update_name = input("Please enter the student's update last name or press enter to keep "
+                             + last_name + ": ")
+
+    if first_update_name == '' and last_update_name == '' or \
+       first_update_name == first_name and last_update_name == last_name:
+        print("\nNo data changed.  Update Cancelled\n")
         return
 
-    students[student_id] = {'first_name': first_update_name, 'last_name': last_update_name}
+    elif first_update_name == '':
+        yes_or_no = dv.get_yes_no(f'\nPlease confirm updating {first_name} {last_name} to {first_name} '
+                                  f'{last_update_name}')
+        if yes_or_no == False:
+            print("No data changed.  Update Cancelled")
+            return
+
+        students[student_id] = {'first_name': first_name, 'last_name': last_update_name}
+
+    elif last_update_name == '':
+        yes_or_no = dv.get_yes_no(f'\nPlease confirm updating {first_name} {last_name} to {first_update_name} '
+                                  f'{last_name}')
+        if yes_or_no == False:
+            print("No data changed.  Update Cancelled")
+            return
+
+        students[student_id] = {'first_name': first_update_name, 'last_name': last_name}
+
+    else:
+        yes_or_no = dv.get_yes_no(f'\nPlease confirm updating {first_name} {last_name} to {first_update_name} '
+                                  f'{last_update_name}')
+        if yes_or_no == False:
+            print("No data changed.  Update Cancelled")
+            return
+
+        students[student_id] = {'first_name': first_update_name, 'last_name': last_update_name}
+
+    print("Update confirm\n")
 
 
 def delete_student(students):
+    if students == {}:
+        print("  There are no students to update")
+        return
+
     print("Delete Students")
+    student_id = dv.get_positive_num("Please enter the ID that is to be updated")
+    if student_id > len(students):
+        print(f'   Student ID{student_id} was not found.')
+        return
+
+    student = students[student_id]
+    first_name, last_name = student.values()
+
+    yes_or_no = dv.get_yes_no(f'Please confirm to delete {first_name} {last_name} ')
+    if yes_or_no == False:
+        print("No data changed.  Delete Cancelled")
+        return
+
+    del students[student_id]
 
 
 if __name__ == '__main__':
