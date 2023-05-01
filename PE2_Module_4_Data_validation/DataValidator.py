046d2c1b0af0602a8a9856487c48b3d1e1ac637c
CreatureCoder <edvahl01@wsc.edu>
2023-05-01 16:11:57 -0500

    Added documention to the DataValidator.py and updated the README.md

--- PE2_Module_4_Data_validation/DataValidator.py
+++ PE2_Module_4_Data_validation/DataValidator.py
@@ -1,7 +1,42 @@
 #!/usr/bin/env python3
 
 """
-password_validator
+data_validator
+
+Assesses data from a file, validate if the data, and then sends the data to different files depending if the data is
+good or not.
+
+Uses a testing_log.txt for the data and a valid_log.txt and an invalid_log.txt to store the data.
+
+Each string of data must consist of 6 parts in the correct formate:
+    id|last name,first name|email|phone number|date|time
+
+Example of valid data:
+    1|Johnson,Debbie|dejohns2@wsc.edu|111-222-3333|12/31/2019|13:40
+
+There are seven different errors the may occur:
+C = invalid data element count
+I = invalid id
+N = invalid name
+E = invalid email
+P = invalid phone number
+D = invalid date
+T = invalid time
+
+Note: if C was the error, there will not be any other errors.
+
+If any of the following errors occur the string will be written to the invalid_log.txt with the type or types of errors
+at the beginning of the string.
+
+Example of written invalid data:
+    IPDT|abc|Johnson|dejohns2@gmail.com|1112223333|12/31/80|1340
+
+If no errors occur then the valid data will be written to the valid_log.txt with some formatting.  The first name will
+be first and last name last, the phone number dashes becomes periods, and the data will be year-month-day.
+
+Example of written valid data:
+    1,Debbie,Johnson,dejohns2@wsc.edu,111.222.3333,2019-12-31,13:40
+
 """
 
 import csv
@@ -14,16 +49,28 @@ __version__ = "1.0"
 __date__ = "2023.04.25"
 __status__ = "development"
 
-
+# stores the errors a string might trigger
 errors = ""
 
 
 def data_validation():
+    """
+    Opens the testing_log.txt, valid_log.txt, and invalid_log.txt and reads and assesses line by line of data.  First
+    test if id is valid, then name is valid, email is valid, phone number is valid, date is valid, and see if time is
+    valid.
+
+    If there is one of the validation fails, add the corresponding errors to the errors and write the entire line to
+    the invalid log with the errors that the data incur.
+
+    If there is nothing in errors, the data is processed and is written to the valid_log
+
+    :return: None
+    """
     global errors
 
     with open('testing_log.txt', mode='r') as testing, \
-         open('invalid_log.txt', 'a', newline='\n') as invalid, \
-         open('valid_log.txt', 'a', newline='\n') as valid:
+            open('valid_log.txt', 'a', newline='\n') as valid, \
+         open('invalid_log.txt', 'a', newline='\n') as invalid:
         valid_writer = csv.writer(valid, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         invalid_writer = csv.writer(invalid, delimiter='|', quoting=csv.QUOTE_MINIMAL)
 
@@ -44,17 +91,28 @@ def data_validation():
                 if len(errors) == 0:
                     name = lines[1]
 
-                    lines[1] = name[name.find(',')+1:] +',' + name[:name.find(',')]
-                    lines[3] = lines[3].replace('-', '.')
-                    date_trans = datetime.datetime.strptime(lines[4], '%m/%d/%Y')
-                    lines[4] = date_trans.strftime("%Y-%m-%d")
+                    # Formats the name
+                    lines[1] = name[name.find(',')+1:]
+                    lines.insert(2, name[:name.find(',')])
+                    # Formats the phone number from dashes to periods
+                    lines[4] = lines[4].replace('-', '.')
+                    # Formats the date form month/day/year to year-month-day
+                    date_trans = datetime.datetime.strptime(lines[5], '%m/%d/%Y')
+                    lines[5] = date_trans.strftime("%Y-%m-%d")
 
                     valid_writer.writerow(lines)
                 else:
                     invalid_writer.writerow([errors] + lines)
+            errors = ""
 
 
 def valid_id(id):
+    """
+    If the supposed id matches with the regex formate, do nothing otherwise add an 'I' to the string of errors
+
+    :param id: Gets the first piece in the string of data which is the supposed id
+    :return: None
+    """
     global errors
     regex = re.compile(r'^[0-9]+$')
 
@@ -63,12 +121,27 @@ def valid_id(id):
 
 
 def valid_name(name):
+    """
+    If the supposed name that has a ',' and matches the regex formate, do nothing otherwise add an 'N' to the string of
+    errors
+
+    :param name: Gets the second piece in the string of data which is the supposed name
+    :return: None
+    """
     global errors
-    if not name.find(',') and not name.isalpha:
+    regex = re.compile(r'^([a-z]+)( [a-z]+)*( [a-z]+)*$')
+
+    if not name.find(',') and re.fullmatch(regex, name):
         errors += "N"
 
 
 def valid_email(email):
+    """
+    If the supposed email matches with the regex formate, do nothing otherwise add an 'E' to the string of errors
+
+    :param email: Gets the third piece in the string of data which is the supposed email
+    :return: None
+    """
     global errors
     regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
@@ -77,6 +150,12 @@ def valid_email(email):
 
 
 def valid_phone(phone):
+    """
+    If the supposed phone matches with the regex formate, do nothing otherwise add an 'P' to the string of errors
+
+    :param phone: Gets the fourth piece in the string of data which is the supposed phone number
+    :return: None
+    """
     global errors
     rule = re.compile(r'\d{3}-\d{3}-\d{4}')
 
@@ -85,6 +164,12 @@ def valid_phone(phone):
 
 
 def valid_date(date):
+    """
+    If the supposed date matches with the regex formate, do nothing otherwise add an 'D' to the string of errors
+
+    :param date: Gets the fifth piece in the string of data which is the supposed date at which was entered
+    :return: None
+    """
     global errors
     date_format = "%m/%d/%Y"
 
@@ -98,6 +183,12 @@ def valid_date(date):
 
 
 def valid_time(time):
+    """
+    If the supposed time matches with the regex formate, do nothing otherwise add an 'T' to the string of errors
+
+    :param time: Gets the last piece in the string of data which is the supposed time at which was entered
+    :return: None
+    """
     global errors
     regex = re.compile(r'([01]?[0-9]|2[0-3]):[0-5][0-9]')
 
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is a respiratory dedicated to Edwin Vahlkamp's Python code
 1. Module_2_SalaryCalculator
 2. Module_4_Student_Maintenance
 3. PE2_Module_2_&_3_PasswordValidator
+4. PE2_Module_4_Data_validator
 
 # Descrition:
 <b>SalaryCalculator: <br></b>
@@ -47,3 +48,39 @@ This is a respiratory dedicated to Edwin Vahlkamp's Python code
 <br>
         End users can deside to activate the DEGUG MODE to see the number of uppercase letters, lowercase letters, digits, valid <br>
         symbols, and the number af characters the password contains. <br>
+
+<b>password_validator</b><br>
+<br>
+Assesses data from a file, validate if the data, and then sends the data to different files depending if the data is
+good or not.<br>
+<br>
+Uses a testing_log.txt for the data and a valid_log.txt and an invalid_log.txt to store the data.<br>
+<br>
+Each string of data must consist of 6 parts in the correct formate:<br>
+    id|last name,first name|email|phone number|date|time<br>
+<br>
+Example of valid data:<br>
+    1|Johnson,Debbie|dejohns2@wsc.edu|111-222-3333|12/31/2019|13:40<br>
+<br>
+There are seven different errors the may occur:<br>
+C = invalid data element count<br>
+I = invalid id<br>
+N = invalid name<br>
+E = invalid email<br>
+P = invalid phone number<br>
+D = invalid date<br>
+T = invalid time<br>
+<br>
+Note: if C was the error, there will not be any other errors.<br>
+<br>
+If any of the following errors occur the string will be written to the invalid_log.txt with the type or types of errors 
+at the beginning of the string.<br>
+<br>
+Example of written invalid data:<br>
+    IPDT|abc|Johnson|dejohns2@gmail.com|1112223333|12/31/80|1340<br>
+<br>
+If no errors occur then the valid data will be written to the valid_log.txt with some formatting.  The first name will 
+be first and last name last, the phone number dashes becomes periods, and the data will be year-month-day.<br>
+<br>
+Example of written valid data:<br>
+    1,Debbie,Johnson,dejohns2@wsc.edu,111.222.3333,2019-12-31,13:40<br>
